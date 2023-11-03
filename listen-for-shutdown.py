de8764faf5474362ee6e2ec4590c951eb1e893b7
Christian George <christian@thegeorge.dk>
2023-11-03 13:30:17 +0100

    Generalized and added logger

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -1,12 +1,43 @@
 #!/usr/bin/env python
 
-
 import RPi.GPIO as GPIO
 import subprocess
+import time
+import logging
+from logging.handlers import RotatingFileHandler
 
+GPIO_PIN=3
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.wait_for_edge(3, GPIO.FALLING)
+def init_gpio():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-subprocess.call(['shutdown', '-h', 'now'], shell=False)
+# Main Loop
+if __name__ == '__main__':
+    log_formatter = logging.Formatter('%(asctime)s  %(filename)s : %(levelname)s  %(message)s')
+    log_filename = '/var/log/power-button.log'
+    log_handler = RotatingFileHandler(log_filename, mode='a', maxBytes=5 * 1024 * 1024,
+                                 backupCount=2, encoding=None, delay=0)
+    log_handler.setFormatter(log_formatter)
+    log_handler.setLevel(logging.INFO)
+    app_log = logging.getLogger('root')
+    app_log.setLevel(logging.DEBUG)
+    app_log.addHandler(log_handler)
+
+    init_gpio()
+
+    while True:
+        count=0
+        app_log.info("Waiting for falling edge on pio:"+str(GPIO_PIN))
+        GPIO.wait_for_edge(GPIO_PIN, GPIO.FALLING)
+        app_log.info("level falling trigged")
+        for x in range(0, 20):
+            time.sleep(0.01)
+            pressed=not GPIO.input(GPIO_PIN)
+            app_log.info("#%d"%x + " pressed:"+str(pressed)+ " pressed_count:%d"%count)
+            if pressed:
+                count+=1
+                if count >= 15:
+                    app_log.info("Calling shutdown pressed_count:%d"%count)
+                    subprocess.call(['shutdown', '-h', 'now'], shell=False)
+                    exit(0)
